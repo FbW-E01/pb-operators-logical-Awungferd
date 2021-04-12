548c0f6156e5a6af1aab7167112cf5e85f0743a0
Ferdinand <ndimbecha@gmail.com>
2021-04-12 11:32:38 +0200

    Do 'And, Not, Or" logical operations

--- log-operators.js
+++ log-operators.js
@@ -1,15 +1,14 @@
 const isDogBetter = true;
 const isCatBetter = false;
-console.log(isDogBetter && isCatBetter);
-console.log(isDogBetter || isCatBetter);
-console.log(isDogBetter && isCatBetter);
-console.log(!(isDogBetter && isCatBetter));
+console.log(isDogBetter && isCatBetter); // Prints 'false' because both variables are not the same
+console.log(isDogBetter || isCatBetter); // JS calculates the variable on the right and returns 'true'
+console.log(!(isDogBetter && isCatBetter)); // The NOT (!) negates the result of the variables in the inner brackets and returns true
 
 const atoms = 237;
 const sandGrains = 400;
 const starsInSky = 500;
-console.log(atoms < starsInSky && atoms < sandGrains);
+console.log(atoms > starsInSky && atoms > sandGrains); //
 console.log(atoms !== sandGrains);
-console.log(starsInSky > sandGrains || starsInSky < atoms);
+console.log(starsInSky < sandGrains || starsInSky > atoms);
 console.log(atoms >= 10 && sandGrains <= 10);
-console.log(atoms * starsInSky > 10 || atoms * sandGrains < 100);
+console.log(atoms * starsInSky < 10 || atoms * sandGrains > 100);
