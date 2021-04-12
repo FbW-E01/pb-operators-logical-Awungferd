e3ba6f9829cd72465257ddecbb2f9050c360dde0
Ferdinand <ndimbecha@gmail.com>
2021-04-12 10:49:57 +0200

    do JS logical operators "AND, OR, NOT"

---
+++ log-operators.js
@@ -0,0 +1,15 @@
+const isDogBetter = "true";
+const isCatBetter = "false";
+console.log(isDogBetter && isCatBetter);
+console.log(isDogBetter || isCatBetter);
+console.log(isDogBetter && isCatBetter);
+console.log(!(isDogBetter && isCatBetter));
+
+const atoms = 237;
+const sandGrains = 400;
+const starsInSky = 500;
+console.log(atoms < starsInSky && atoms < sandGrains);
+console.log(atoms !== sandGrains);
+console.log(starsInSky > sandGrains || starsInSky < atoms);
+console.log(atoms >= 10 && sandGrains <= 10);
+console.log(atoms * starsInSky > 10 || atoms * sandGrains < 100);
