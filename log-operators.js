0521d4f68ffe25f2a5f7c49f2a9c5340e27e12c7
Ferdinand <ndimbecha@gmail.com>
2021-04-12 11:42:17 +0200

    Not, Or, And as logical operators in JS

--- log-operators.js
+++ log-operators.js
@@ -8,7 +8,7 @@ const atoms = 237;
 const sandGrains = 400;
 const starsInSky = 500;
 console.log(atoms > starsInSky && atoms > sandGrains); //
-console.log(atoms !== sandGrains);
-console.log(starsInSky < sandGrains || starsInSky > atoms);
-console.log(atoms >= 10 && sandGrains <= 10);
-console.log(atoms * starsInSky < 10 || atoms * sandGrains > 100);
+console.log(atoms !== sandGrains); // returns true as both are not exactly equal
+console.log(starsInSky < sandGrains || starsInSky > atoms); // One declaration is true so true is returned
+console.log(atoms >= 10 && sandGrains <= 10); //both declarations are false, hence false is printed
+console.log(atoms * starsInSky < 100 || atoms * sandGrains > 100); // the second declaration is true, so it overrides all other declarations and returns true
